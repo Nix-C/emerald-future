2eadceedaf3fff46ce3b1af5873ecab9c1ba79f2
Nathaniel Campbell <nixc.web@gmail.com>
2026-08-23 20:50:43 -0500

    Finished adding AncientLake_Exit

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1744,3 +1744,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/AncientLake_Entrance/scripts.inc"
 
 	.include "data/maps/DeeprootVillage_DeeprootVillage/scripts.inc"
+
+	.include "data/maps/AncientLake_Exit/scripts.inc"
