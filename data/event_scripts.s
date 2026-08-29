47a464966acb8ec406d59513809990c12673e4a8
Nathaniel Campbell <nixc.web@gmail.com>
2026-08-28 22:41:30 -0500

    Finish adding DeeprootFarm

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1752,3 +1752,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/RockCity_RockCity/scripts.inc"
 
 	.include "data/maps/SouthernForest_SouthernForest/scripts.inc"
+
+	.include "data/maps/DeeprootVillage_DeeprootFarm/scripts.inc"
